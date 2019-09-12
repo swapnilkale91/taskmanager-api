f4f20eab10f664e695b261e19c901e47fa9794a5
Kale <SKale@thegoldensource.com>
2019-09-13 00:00:59 +0530

    Aligning jwt settings

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,7 +9,7 @@ const auth = async (req, res, next) => {
         console.log('get header : ' + req.header('Authorization'))
         const token = req.header('Authorization').replace('Bearer ','')
         console.log('token ' + token)
-        const decoded = jwt.verify(token, JWTSECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         console.log('decoded id..' + decoded._id)
         const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
         console.log('user..' + user)
